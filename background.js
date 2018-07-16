5c7aeb1651069d1d6e1248cd53556f6e69821652
Mfweb <h@mfweb.pw>
2018-07-16 19:49:15 +0800

    添加一些注释

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 var cdn_now = false;
 
+/**
+ * 初始化读取CDN设置
+ */
 chrome.storage.sync.get(['cdnsta'], function(result) {
 	var now_cdn = result.cdnsta;
 	if(now_cdn == undefined  || now_cdn == null || now_cdn == 'off'){
@@ -10,6 +13,11 @@ chrome.storage.sync.get(['cdnsta'], function(result) {
 	}
 });
 
+/**
+ * 与前台脚本通讯
+ * 显示岛上当前页面GIF数量
+ * 接收CDN重定向设置
+ */
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if(sender.tab)
@@ -23,24 +31,28 @@ chrome.runtime.onMessage.addListener(
 		}	
 });
 
-
+/**
+ * Chrome发出网络请求之前调用
+ * 将GIF图片重定向到原图（岛上有些图虽然是GIF，但是后缀不是GIF，这些图无法被统计和替换）
+ * 如果启用则将所有图片重定向到《a岛.加藤惠.我爱你》
+ */
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details) {
 		var rcurl = details.url;
 		if(rcurl.match(/(nmbimg|tnmbstatic)\.fastmirror\.org(\/Public\/Upload)*\/(thumb)|(image)\//ig)) {
 			if(cdn_now){
-				rcurl = rcurl.replace("nmbimg.fastmirror.org", encodeURI("a岛.加藤惠.我爱你/adimg/?img="));
+				rcurl = rcurl.replace("nmbimg.fastmirror.org", "a岛.加藤惠.我爱你/adimg/?img=");
 				rcurl = rcurl.replace("http:", "https:");
 			}
-
 			if(rcurl.match(/(\.gif)$/i)) {
-				return {redirectUrl: rcurl.replace("/thumb/","/image/")};
+				rcurl = rcurl.replace("/thumb/","/image/");
 			}
 			console.log(rcurl);
+			return {redirectUrl: rcurl};
 		}
-		return {redirectUrl: rcurl};
 	},
 	{
+		//过滤出主岛图片CDN和备胎岛图片CDN
 		urls: ["*://nmbimg.fastmirror.org/*","*://tnmbstatic.fastmirror.org/*"],
 		types:["image"]
 	},
--- script.js
+++ script.js
@@ -1,21 +1,16 @@
+/**
+ * 这个脚本将注入到主岛和备胎岛的页面里
+ * 现在只用来统计当前页面GIF图片数量并发送到前台显示
+ */
+
 var images = document.getElementsByTagName("img");
-//http://.fastmirror.org/Public/Upload/thumb/2018-03-31/5abf11a2bca05.gif"
-var te = /(nmbimg|tnmbstatic)\.fastmirror\.org(\/Public\/Upload)*\/thumb\//g;
-var ext = /(\.gif)$/;
 var counter = 0;
 for(let i = 0;i < images.length;i++)
 {
 	var temp_img_url = images[i].src;
-	if(temp_img_url.match(te) && temp_img_url.match(ext))
-	{
+	if(temp_img_url.match(/(nmbimg|tnmbstatic)\.fastmirror\.org(\/Public\/Upload)*\/thumb\//ig) && 
+		temp_img_url.match(/(\.gif)$/ig)) {
 		counter++;
-		/*iimages[i].src = temp_img_url.replace("/thumb/","/image/");
-		mages[i].onload = function(){
-			if(!this.src.match(te))return;
-			var imgurl = this.src;
-			this.src = imgurl.replace("/thumb/","/image/");
-		};*/
-		//console.log(images[i].src);
 	}
 }
 
