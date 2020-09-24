8e084e648a854e1bd5e7014c7653cf403f2c1619
Mfweb <h@i2s.io>
2020-09-25 07:50:22 +0800

    兼容adnmb3

--- background.js
+++ background.js
@@ -3,12 +3,12 @@ var cdn_now = false;
 /**
  * 初始化读取CDN设置
  */
-chrome.storage.sync.get(['cdnsta'], function(result) {
+chrome.storage.sync.get(['cdnsta'], function (result) {
 	var now_cdn = result.cdnsta;
-	if(now_cdn == undefined  || now_cdn == null || now_cdn == 'off'){
+	if (now_cdn == undefined || now_cdn == null || now_cdn == 'off') {
 		cdn_now = false;
 	}
-	else{
+	else {
 		cdn_now = true;
 	}
 });
@@ -19,17 +19,16 @@ chrome.storage.sync.get(['cdnsta'], function(result) {
  * 接收CDN重定向设置
  */
 chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		if(sender.tab)
-		{
-			var dt = {text:String(request.counter),tabId:sender.tab.id};
+	function (request, sender, sendResponse) {
+		if (sender.tab) {
+			var dt = { text: String(request.counter), tabId: sender.tab.id };
 			chrome.browserAction.setBadgeText(dt);
 		}
-		else{
+		else {
 			cdn_now = request.cdn;
 			console.log(cdn_now);
-		}	
-});
+		}
+	});
 
 /**
  * Chrome发出网络请求之前调用
@@ -37,24 +36,24 @@ chrome.runtime.onMessage.addListener(
  * 如果启用则将所有图片重定向到《a岛.加藤惠.我爱你》
  */
 chrome.webRequest.onBeforeRequest.addListener(
-	function(details) {
+	function (details) {
 		var rcurl = details.url;
-		if(rcurl.match(/(nmbimg|tnmbstatic)\.fastmirror\.org(\/Public\/Upload)*\/(thumb)|(image)\//ig)) {
+		if (rcurl.match(/(nmbimg|tnmbstatic)\.fastmirror\.org(\/Public\/Upload)*\/(thumb)|(image)\//ig)) {
 			/*if(cdn_now){
 				rcurl = rcurl.replace("nmbimg.fastmirror.org", "a岛.加藤惠.我爱你/adimg/?img=");
 				rcurl = rcurl.replace("http:", "https:");
 			}*/
-			if(rcurl.match(/(\.gif)$/i)) {
-				rcurl = rcurl.replace("/thumb/","/image/");
+			if (rcurl.match(/(\.gif)$/i)) {
+				rcurl = rcurl.replace("/thumb/", "/image/");
 			}
 			console.log(rcurl);
-			return {redirectUrl: rcurl};
+			return { redirectUrl: rcurl };
 		}
 	},
 	{
 		//过滤出主岛图片CDN和备胎岛图片CDN
-		urls: ["*://nmbimg.fastmirror.org/*","*://tnmbstatic.fastmirror.org/*"],
-		types:["image"]
+		urls: ["*://nmbimg.fastmirror.org/*", "*://tnmbstatic.fastmirror.org/*"],
+		types: ["image"]
 	},
 	["blocking"]
 );
